2a572a53ec6a08e82aad9919c749c6c8611f2607
Abhinav Tiwari <tiwariabhinavin@gmail.com>
2020-11-30 20:03:03 +0530

    Customized blogs page and added facebook plugin

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,18 +1,23 @@
 import Markdown from "./Markdown";
 
 import React, { useState, useEffect } from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Image } from "react-bootstrap";
 import { useParams } from "react-router";
+import blogs from "../content/blogs.json";
+import Typography from "@material-ui/core/Typography";
 
 function Blog() {
   const [blog, setBlog] = useState("");
   const [address, setAddress] = useState("");
+  const [blogMeta, setMeta] = useState({});
+
   let { id } = useParams();
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
+    setMeta(blogs.blogs[id - 1]);
     const readmePath = require(`../content/${id}.md`);
-    setAddress("http://localhost:3000/blog/" + id);
+    setAddress("https://sch-iitk.github.io/#/blog/" + id);
     fetch(readmePath.default)
       .then((response) => response.text())
       .then((text) => {
@@ -22,29 +27,51 @@ function Blog() {
 
   return (
     <Container className="mt-5">
+      <Row className="justify-content-md-center ">
+        <Col xs={12} md={8}>
+          <h1>{blogMeta.Header}</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center ">
+        <Col xs={10} md={8}>
+          <Typography variant="button" display="block" gutterBottom>
+            {blogMeta.Name}
+          </Typography>
+
+          <Typography variant="button"> {" " + blogMeta.time}</Typography>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center ">
+        <Col xs={12} md={8}>
+          <Image src={blogMeta.image} rounded width={"100%"} height="auto" />
+        </Col>
+      </Row>
       <Row className="justify-content-md-center p-2">
-        <Col md={6} className="justify-content-center">
+        <Col md={8} className="justify-content-center">
           <Markdown className="blog">{blog}</Markdown>
         </Col>
       </Row>
-      <Row className="justify-content-md-center  justify-content-xs-center mt-5">
-        <div
-          class="fb-like"
-          data-href={address}
-          data-width=""
-          data-layout="standard"
-          data-action="like"
-          data-size="small"
-          data-share="true"
-        ></div>
+      <Row className="justify-content-md-center   mt-5">
+        <Col xs={10} md={8}>
+          <div
+            class="fb-like"
+            data-href={address}
+            layout="button_count"
+            data-action="like"
+            data-size="small"
+            data-share="true"
+          ></div>
+        </Col>
       </Row>
-      <Row className="justify-content-md-center  justify-content-xs-center">
-        <div
-          class="fb-comments"
-          data-href={address}
-          data-numposts="5"
-          data-width=""
-        ></div>
+      <Row className="justify-content-md-center  ">
+        <Col xs={10} md={8}>
+          <div
+            class="fb-comments"
+            data-href={address}
+            data-numposts="5"
+            data-width=""
+          ></div>
+        </Col>
       </Row>
     </Container>
   );
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+
 import { Container, Row, Col } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import blogs from "../content/blogs.json";
